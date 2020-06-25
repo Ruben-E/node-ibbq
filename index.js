f6949a21dc1bb5ddaaf59122472541db6c79ebfe
Ruben Ernst <rubenernst@gmail.com>
2020-06-25 09:04:32 +0200

    Initial implementation of the login step with the iBBQ device

--- index.js
+++ index.js
@@ -15,7 +15,20 @@ noble.on('discover', function (peripheral) {
     if (peripheral.advertisement.localName === "iBBQ") {
         noble.stopScanning();
         peripheral.connect(function (error) {
+            console.log("Connected!")
+            peripheral.discoverAllServicesAndCharacteristics(function (error, services, characteristics) {
+                console.log('Discovered services and characteristics', services, characteristics);
 
+                for (const characteristic of characteristics) {
+                    if (characteristic.uuid === 'fff2') {
+                        console.log("Found login characteristic");
+                        const data = Buffer.from([0x21, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0xb8, 0x22, 0x00, 0x00, 0x00, 0x00, 0x00])
+                        characteristic.write(data, false, function (err) {
+                            console.log(err);
+                        });
+                    }
+                }
+            })
         });
     }
 });
